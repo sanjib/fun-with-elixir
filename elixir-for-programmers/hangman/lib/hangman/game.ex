91a2fedf438d92b0dfb57de7e421d571459e4fb8
Sanjib Ahmad <sanjib@sanjib.org>
2020-05-29 13:27:54 +0800

    Defdelegate make_move

--- elixir-for-programmers/hangman/lib/hangman/game.ex
+++ elixir-for-programmers/hangman/lib/hangman/game.ex
@@ -20,10 +20,12 @@ defmodule Hangman.Game do
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
     game
+    |> return_with_tally()
   end
 
   def make_move(game, guess) do
     accept_move(game, guess, MapSet.member?(game.used, guess))
+    |> return_with_tally()
   end
 
   def tally(game) do
@@ -36,6 +38,8 @@ defmodule Hangman.Game do
   end
 
   ###################### ↓↓↓ PRIVATE ↓↓↓ ######################
+
+  defp return_with_tally(game), do: {game, tally(game)}
 
   defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
