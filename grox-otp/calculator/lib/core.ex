00e599929a4aaff18e505015f3c493de9c00148f
Sanjib Ahmad <sanjib@sanjib.org>
2020-06-03 00:37:54 +0800

    Init grox course on OTP

---
+++ grox-otp/calculator/lib/core.ex
@@ -0,0 +1,10 @@
+defmodule Calculator.Core do
+  def add(acc, num), do: acc + num
+  def subtract(acc, num), do: acc - num
+  def multiply(acc, num), do: acc * num
+  def divide(acc, num), do: acc / num
+
+  def fold(list, acc, fun) do
+    Enum.reduce(list, acc, fn x, acc -> fun.(acc, x) end)
+  end
+end
--- programming-elixir-1-6/ch2/exercise-pattern-matching.exs
+++ programming-elixir-1-6/ch2/exercise-pattern-matching.exs
@@ -1,3 +1,5 @@
+# -- Exercise 1
+
 # will match
 a = 4
 a = [[1, 2, 3]]
@@ -8,3 +10,16 @@ a = [1, 2, 3]
 4 = a
 [a, b] = [1, 2, 3]
 [[a]] = [[1, 2, 3]]
+
+# -- Exercise 2
+
+# will match
+a = 1
+^a = 1
+^a = 2 - a
+
+# will not match
+[a, b, a] = [1, 2, 3]
+[a, b, a] = [1, 1, 2]
+^a = 2
+
