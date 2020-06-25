5c6a693bbe425cc4e624f5ea78022ff180c9335c
Sanjib Ahmad <sanjib@sanjib.org>
2020-06-26 02:25:21 +0800

    Implement bingo checker list generator algorithm

--- pragstudio-bingo/bingo/lib/bingo/bingo_checker.ex
+++ pragstudio-bingo/bingo/lib/bingo/bingo_checker.ex
@@ -1,4 +1,42 @@
 defmodule Bingo.BingoChecker do
   @moduledoc "Determines if a bingo has been achieved by examining squares"
 
+  def bingo?(_squares) do
+
+  end
+
+  def row_list(size) do
+    Enum.reduce(0..(size-1), [], fn row, acc ->
+      start = size * row
+      finish = start + (size-1)
+      head = for col <- start..finish do
+        col
+      end
+      [head | acc]
+    end)
+    |> Enum.sort
+  end
+
+  def col_list(size) do
+    Enum.reduce(0..(size-1), [], fn row, acc ->
+      head = for col <- 0..(size-1) do
+        row + (size * col)
+      end
+      [head | acc]
+    end)
+    |> Enum.sort
+  end
+
+  def diag_list(size) do
+    multiplier = [size + 1, size - 1]
+    Enum.reduce(0..1, [], fn row, acc ->
+      start = row
+      finish = start + (size-1)
+      head = for col <- start..finish do
+        col * Enum.at(multiplier, row)
+      end
+      [head | acc]
+    end)
+    |> Enum.sort
+  end
 end
--- pragstudio-bingo/bingo/test/bingo_test.exs
+++ pragstudio-bingo/bingo/test/bingo_test.exs
@@ -13,8 +13,36 @@ defmodule BingoTest do
     assert total_size == size * size
   end
 
-  test "test marked by" do
-
+  test "bing checker row list" do
+    size = 5
+    expected = [
+      [ 0,  1,  2,  3,  4],
+      [ 5,  6,  7,  8,  9],
+      [10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24],
+    ]
+    assert expected == Bingo.BingoChecker.row_list(size)
   end
 
+  test "bing checker col list" do
+    size = 5
+    expected = [
+      [ 0,  5, 10, 15, 20],
+      [ 1,  6, 11, 16, 21],
+      [ 2,  7, 12, 17, 22],
+      [ 3,  8, 13, 18, 23],
+      [ 4,  9, 14, 19, 24],
+    ]
+    assert expected == Bingo.BingoChecker.col_list(size)
+  end
+
+  test "bing checker diag list" do
+    size = 5
+    expected = [
+      [ 0,  6, 12, 18, 24],
+      [ 4,  8, 12, 16, 20],
+    ]
+    assert expected == Bingo.BingoChecker.diag_list(size)
+  end
 end
