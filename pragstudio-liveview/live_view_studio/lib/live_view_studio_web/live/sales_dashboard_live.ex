e23c4d512f1b1aa958f13eccf619ffb9753361cb
Sanjib Ahmad <sanjib@sanjib.org>
2020-07-08 13:03:44 +0800

    Implement sales dashboard refresh interval options

--- pragstudio-liveview/live_view_studio/lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ pragstudio-liveview/live_view_studio/lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -6,13 +6,27 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+#    if connected?(socket) do
+#      tref = :timer.send_interval(1000, self(), :tick)
+#      IO.puts "--> timer ref #{inspect tref}"
+#    end
 
+    initial_refresh_interval = 3
+
+    # Don't call LiveViewStudio.Sales initially because mount
+    # gets called twice initially, first with the static content
+    # then one more time with the join & reply socket calls
     socket = assign(socket,
-      new_orders: Sales.new_orders(),
-      sales_amount: Sales.sales_amount(),
-      satisfaction: Sales.satisfaction()
+      new_orders: 0,
+      sales_amount: 0,
+      satisfaction: 0,
+      refresh_interval: initial_refresh_interval
     )
+
+    if connected?(socket) do
+      schedule_refresh(socket)
+    end
+
     {:ok, socket}
   end
 
@@ -35,24 +49,53 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
         </div>
       </div>
       <br>
+
+      <form phx-change="update_refresh_interval">
+        <select name="refresh_interval">
+          <%= options_for_select(refresh_options, @refresh_interval) %>
+        </select>
+      </form>
+
       <button phx-click="refresh">Refresh</button>
+
     </div>
 
     """
   end
 
   def handle_event("refresh", _, socket) do
-    {:noreply, update_stats(socket)}
+    socket = update_stats(socket)
+    {:noreply, socket}
+  end
+
+  def handle_event("update_refresh_interval", %{"refresh_interval" => refresh_interval}, socket) do
+    refresh_interval = String.to_integer(refresh_interval)
+    IO.puts "--> updated refresh interval: #{refresh_interval}"
+    socket = assign(socket, :refresh_interval, refresh_interval)
+    {:noreply, socket}
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, update_stats(socket)}
+    socket = update_stats(socket)
+    {:noreply, socket}
   end
 
   defp update_stats(socket) do
+    schedule_refresh(socket)
+
     socket
     |> update(:new_orders, &(&1 + Sales.new_orders))
     |> update(:sales_amount, &(&1 + Sales.sales_amount))
     |> assign(:satisfaction, Sales.satisfaction)
+  end
+
+  defp refresh_options do
+    [{"1s", 1}, {"2s", 2}, {"3s", 3}, {"5s", 5}, {"10s", 10}, {"15s", 15}, {"30s", 30}, {"60s", 60}]
+  end
+
+  defp schedule_refresh(socket) do
+    refresh_interval = socket.assigns.refresh_interval
+    IO.puts "--> refresh interval: #{refresh_interval}"
+    Process.send_after(self(), :tick, refresh_interval * 1000)
   end
 end
