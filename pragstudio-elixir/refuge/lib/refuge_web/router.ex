0aae55201fda998c61cbb5237ac6ea8520102bf7
Sanjib Ahmad <sanjib@sanjib.org>
2020-10-29 13:07:14 +0800

    Update npm packages

--- pragstudio-elixir/refuge/lib/refuge_web/router.ex
+++ pragstudio-elixir/refuge/lib/refuge_web/router.ex
@@ -2,24 +2,26 @@ defmodule RefugeWeb.Router do
   use RefugeWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", RefugeWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-#    get "/bears", BearController, :index
-#    get "/", PageController, :index
+    #    get "/bears", BearController, :index
+    # get("/", PageController, :index)
 
-    resources "/bears", BearController
+    get("/", BearController, :index)
+
+    resources("/bears", BearController)
   end
 
   # Other scopes may use custom stacks.
@@ -38,8 +40,8 @@ defmodule RefugeWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: RefugeWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: RefugeWeb.Telemetry)
     end
   end
 end
